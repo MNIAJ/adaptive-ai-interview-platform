9501436368768cf15246ef65062518643f8b5385
MNIAJ <ayushjoshi310@gmail.com>
2026-09-25 17:13:42 +0530

    Update application entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,20 +7,22 @@ import Register from './pages/Register.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import Interview from './pages/Interview.jsx'
 import Report from './pages/Report.jsx'
+import History from './pages/History.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
 import Layout from './components/Layout.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<ProtectedRoute><Layout><Dashboard /></Layout></ProtectedRoute>} />
-          <Route path="/interview/:sessionId" element={<ProtectedRoute><Layout><Interview /></Layout></ProtectedRoute>} />
-          <Route path="/report/:sessionId" element={<ProtectedRoute><Layout><Report /></Layout></ProtectedRoute>} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<ProtectedRoute><Layout><Dashboard /></Layout></ProtectedRoute>} />
+                <Route path="/history" element={<ProtectedRoute><Layout><History /></Layout></ProtectedRoute>} />
+                <Route path="/interview/:sessionId" element={<ProtectedRoute><Layout><Interview /></Layout></ProtectedRoute>} />
+                <Route path="/report/:sessionId" element={<ProtectedRoute><Layout><Report /></Layout></ProtectedRoute>} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>,
 )
